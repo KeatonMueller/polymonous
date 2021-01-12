b3218feaec12ec536960b0c4aa768c68dd28bbd9
Keaton Mueller <keaton.mueller@yale.edu>
2021-01-12 01:02:16 -0500

    switch back to GDScript

--- MainScene.gd
+++ MainScene.gd
@@ -2,95 +2,191 @@ extends Node2D
 
 # possible actions
 const Action = {
-	"WORLD_ROT_LEFT": 0,
-	"WORLD_ROT_RIGHT": 1,
-	"DROP": 2,
+	"ROT_LEFT": "rot_left",
+	"ROT_RIGHT": "rot_right",
+	"DROP": "drop",
 };
 
-# possible directions
+# directions for a given action
 const Direction = {
-	Action.WORLD_ROT_LEFT: -1,
-	Action.WORLD_ROT_RIGHT: 1
+	Action.ROT_LEFT: -1,
+	Action.ROT_RIGHT: 1
 }
 
+const ROT_SPEED: float = 0.2;
+const PRECISION: int = 6;
+const INITIAL_HEIGHT: float = 200.0;
+
 const Box = preload("res://Box.tscn");
+onready var tw: Tween = get_node("Tween");
+var base: StaticBody2D;
+var cam: Camera2D;
+var anim: AnimationPlayer;
+
 # current box values
-var curr_box : KinematicBody2D;
-var curr_theta : float = -PI / 2;
-var curr_height : float = 200.0;
+var curr_box: KinematicBody2D;
+var theta_calc: float = 0;
+var theta_offset: float = -PI / 2;
+var theta_display: float = theta_calc + theta_offset;
+var curr_height: float = 200.0;
 
-# values for base
-var base : StaticBody2D;
-var rot_delta : float;
-var rot_speed : float;
-
-# values for actions
-var dist_left : float = 0;
-var drop_dir : Vector2 = Vector2();
-var drop_speed : float = 4;
-var rot_left : float = 0;
-var rot_dir : int = 0;
-var action_list : Array = [];
-var next_action : int;
+var drop_vector: Vector2 = Vector2();
+var drop_speed: float = 5.0;
+var dropping: bool = false;
+var action_list: Array = [];
+var next_action: String;
+var tweening: bool = false;
+var Adjacent: Dictionary = {};
+var Wrap: Dictionary = {};
+var boxes: Dictionary = {};
+var last_dir: int;
 
 func _ready():
-	# get base
+	tw = get_node("Tween");
 	base = get_node("Base");
-	rot_delta = base.rot_delta;
-	rot_speed = base.rot_speed;
-	add_child(base);
+	cam = base.get_node("Camera2D");
+	calc_thetas(base.num_sides);
 	# load initial box
-	curr_box = Box.instance();
-	curr_box.position.x = cos(curr_theta) * curr_height;
-	curr_box.position.y = sin(curr_theta) * curr_height;
-	add_child(curr_box);
+	new_box();
 
 func _physics_process(delta):
-	# on button press, add to list of actions
-	if Input.is_action_just_pressed("world_rot_right"):
-		action_list.append(Action.WORLD_ROT_RIGHT);
-	elif Input.is_action_just_pressed("world_rot_left"):
-		action_list.append(Action.WORLD_ROT_LEFT);
-	elif Input.is_action_just_pressed("box_drop"):
-		action_list.append(Action.DROP);
+	for action in Action.values():
+		if Input.is_action_just_pressed(action):
+			action_list.append(action);
+			break;
 	
-	# get next action if any are pending
-	if action_list.size() > 0 and rot_left == 0 and dist_left == 0:
+	if action_list.size() > 0 and not tweening and not dropping:
 		next_action = action_list.pop_front();
 		if next_action != Action.DROP:
-			rot_left = rot_delta;
-			rot_dir = Direction[next_action];
+			rotate(Direction[next_action]);
 		else:
-			dist_left = curr_box.position.distance_to(base.position);
-			drop_dir.x = cos(curr_theta) * -dist_left * delta * drop_speed;
-			drop_dir.y = sin(curr_theta) * -dist_left * delta * drop_speed;
-		
-	# reposition and rotate box
-	if rot_left > 0:
-		var rot_amt = rot_delta * delta * rot_speed;
-		# check for over-rotating
-		if rot_left - rot_amt < 0:
-			rot_amt = rot_left;
-		
-		# update theta and position
-		curr_theta += rot_amt * rot_dir;
-		curr_theta = wrapf(curr_theta, -PI, PI);
-		curr_box.position.x = cos(curr_theta) * curr_height;
-		curr_box.position.y = sin(curr_theta) * curr_height;
-		# rotate box so it appear stationary
-		curr_box.rotate(rot_dir * rot_amt);
-		
-		# update rot_left
-		rot_left -= rot_amt;
-		
-	# drop box
-	if dist_left > 0:
-		var r = curr_box.move_and_collide(drop_dir);
-		if r != null and r.collider != null:
-			dist_left = 0;
-			curr_box.deactivate();
-			# initialize a new box
-			curr_box = Box.instance();
-			curr_box.position.x = cos(curr_theta) * curr_height;
-			curr_box.position.y = sin(curr_theta) * curr_height;
-			add_child(curr_box);
+			drop();
+
+	if tweening:
+		theta_display = theta_calc + theta_offset;
+		curr_box.position.x = cos(theta_display) * curr_height;
+		curr_box.position.y = sin(theta_display) * curr_height;
+
+	if dropping:
+		var move_vector = Vector2(drop_vector);
+		move_vector.x *= delta * drop_speed;
+		move_vector.y *= delta * drop_speed;
+		if curr_box.move_and_collide(move_vector) != null:
+			dropping = false;
+			lock_box();
+			new_box();
+
+func lock_box():
+	var key = approx(theta_display, PRECISION);
+	if boxes.has(key):
+		boxes[key].append(curr_box);
+	else:
+		boxes[key] = [curr_box];
+
+	if boxes.size() == base.num_sides:
+		for theta in boxes.keys():
+			remove_child(boxes[theta].pop_front());
+			if boxes[theta].size() == 0:
+				boxes.erase(theta);
+
+func new_box():
+	curr_box = Box.instance();
+	anim = curr_box.get_node("AnimationPlayer");
+	curr_height = INITIAL_HEIGHT;
+	var new_pos = Vector2();
+	new_pos.x = cos(theta_display) * curr_height;
+	new_pos.y = sin(theta_display) * curr_height;
+	curr_box.position = new_pos;
+	curr_box.rotation = theta_calc;
+	add_child(curr_box);
+	
+func calc_thetas(num_sides):
+	var rot_delta = 2 * PI / num_sides;
+	var left_dir = Direction[Action.ROT_LEFT];
+	var right_dir = Direction[Action.ROT_RIGHT];
+	var rotations: Array = [];
+	Adjacent[left_dir] = {};
+	Adjacent[right_dir] = {};
+	var theta = -rot_delta;
+	for _i in range(num_sides + 2):
+		rotations.append(approx(theta, PRECISION));
+		theta += rot_delta;
+	for i in range(num_sides + 1):
+		Adjacent[right_dir][rotations[i]] = rotations[i + 1];
+		Adjacent[left_dir][rotations[i + 1]] = rotations[i];
+	Wrap.clear();
+	Wrap[rotations[0]] = rotations[num_sides];
+	Wrap[rotations[num_sides + 1]] = rotations[1];
+
+func drop():
+	var dist = curr_box.position.distance_to(base.position);
+	drop_vector.x = cos(theta_display) * -dist;
+	drop_vector.y = sin(theta_display) * -dist;
+	dropping = true;
+
+func rotate(rot_dir):
+	var box_rot = approx(curr_box.rotation, PRECISION);
+	var next_box_rot = Adjacent[rot_dir][box_rot];
+	tw.interpolate_property(
+		curr_box,
+		"rotation",
+		curr_box.rotation,
+		next_box_rot,
+		ROT_SPEED,
+		Tween.TRANS_SINE,
+		Tween.EASE_IN_OUT
+	);
+
+	var th = approx(theta_calc, PRECISION);
+	var next_th = Adjacent[rot_dir][th];
+	tw.interpolate_property(
+		self,
+		"theta_calc",
+		theta_calc,
+		next_th,
+		ROT_SPEED,
+		Tween.TRANS_SINE,
+		Tween.EASE_IN_OUT
+	);
+
+	var cam_rot = approx(cam.rotation, PRECISION);
+	var next_cam_rot = Adjacent[rot_dir][cam_rot];
+	tw.interpolate_property(
+		cam,
+		"rotation",
+		cam.rotation,
+		next_cam_rot,
+		ROT_SPEED,
+		Tween.TRANS_CUBIC,
+		Tween.EASE_OUT
+	);
+
+	tweening = true;
+	tw.start();
+	last_dir = rot_dir;
+
+func _on_Tween_tween_completed(object, key):
+	if object == curr_box and key == ":rotation":
+		var rot = approx(curr_box.rotation, PRECISION);
+		if Wrap.has(rot):
+			curr_box.rotation = Wrap[rot];
+	elif object == self and key == ":theta_calc":
+		var th = approx(theta_calc, PRECISION);
+		if Wrap.has(th):
+			theta_calc = Wrap[th];
+		theta_display = theta_calc + theta_offset;
+		if last_dir == Direction[Action.ROT_LEFT]:
+			anim.play("sway_left");
+		elif last_dir == Direction[Action.ROT_RIGHT]:
+			anim.play("sway_right");
+	elif object == cam and key == ":rotation":
+		var rot = approx(cam.rotation, PRECISION);
+		if Wrap.has(rot):
+			cam.rotation = Wrap[rot];
+
+func _on_Tween_tween_all_completed():
+	tweening = false;
+
+func approx(num, precision):
+	return stepify(num, pow(10, -precision));
+
